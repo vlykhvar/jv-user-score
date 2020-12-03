152276c4369f2edcdb09e59ba36cfbe6fca794ba
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-03 18:10:22 +0200

    v1.2 deleted varible; fit code bend the knee

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,15 +4,12 @@ import core.basesyntax.exception.UserNotFoundException;
 
 public class UserService {
     public int getUserScore(String[] records, String email) {
-        String getRecods = "";
         for (String record : records) {
             if (email.equals(record.split(":")[0])) {
-                getRecods = record;
+
+                return Integer.parseInt(record.substring(record.indexOf(':') + 1));
             }
         }
-        if (getRecods.equals("")) {
-            throw new UserNotFoundException("User with given email doesn't exist");
-        }
-        return Integer.parseInt(getRecods.substring(getRecods.indexOf(':') + 1));
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
