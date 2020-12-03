20bcce77e37a8a2662a526a7c0dae6df31d8dba4
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-03 13:06:20 +0200

    v1.0 has written UserService.java has written UserNotFoundException.java

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
 
-    public int getUserScore(String[] records, String email) {
-        return 0;
+    public int getUserScore(String[] records, String email) throws UserNotFoundException {
+        if (!isExit(records, email)) {
+            throw new UserNotFoundException("User with given email doesn't exist");
+        }
+        return getRecords(records, email);
+    }
+
+    private boolean isExit(String[] records, String email) {
+        for (String record : records) {
+            if (email.equals(record.split(":")[0])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getRecords(String[] records, String email) {
+        String getRecods = "";
+        for (String record : records) {
+            if (email.equals(record.split(":")[0])) {
+                getRecods = record;
+                break;
+            }
+        }
+        return Integer.parseInt(getRecods.substring(getRecods.indexOf(':') + 1));
     }
 }
---
+++ src/main/java/core/basesyntax/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package core.basesyntax.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
