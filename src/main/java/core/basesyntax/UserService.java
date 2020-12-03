3bd596e94d011c41803c83f39a5e27e175995bc4
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-03 15:06:38 +0200

    v1.1 has rewritten getUserScore

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,29 +4,15 @@ import core.basesyntax.exception.UserNotFoundException;
 
 public class UserService {
 
-    public int getUserScore(String[] records, String email) throws UserNotFoundException {
-        if (!isExit(records, email)) {
-            throw new UserNotFoundException("User with given email doesn't exist");
-        }
-        return getRecords(records, email);
-    }
-
-    private boolean isExit(String[] records, String email) {
-        for (String record : records) {
-            if (email.equals(record.split(":")[0])) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private int getRecords(String[] records, String email) {
+    public int getUserScore(String[] records, String email) {
         String getRecods = "";
         for (String record : records) {
             if (email.equals(record.split(":")[0])) {
                 getRecods = record;
-                break;
             }
+        }
+        if (getRecods.equals("")) {
+            throw new UserNotFoundException("User with given email doesn't exist");
         }
         return Integer.parseInt(getRecods.substring(getRecods.indexOf(':') + 1));
     }
