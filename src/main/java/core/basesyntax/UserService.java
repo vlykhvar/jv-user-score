9e8bc245764d9aa9ed7946a973d97686faf1da46
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-03 18:14:47 +0200

    v1.2.1 deleted one space line

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,7 +6,6 @@ public class UserService {
     public int getUserScore(String[] records, String email) {
         for (String record : records) {
             if (email.equals(record.split(":")[0])) {
-
                 return Integer.parseInt(record.substring(record.indexOf(':') + 1));
             }
         }
