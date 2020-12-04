a39eb19c4a219515ee1a37e1eaa866ddc91e2c9c
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-04 11:37:17 +0200

    v1.2.1 got fatter

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,8 +5,9 @@ import core.basesyntax.exception.UserNotFoundException;
 public class UserService {
     public int getUserScore(String[] records, String email) {
         for (String record : records) {
-            if (email.equals(record.split(":")[0])) {
-                return Integer.parseInt(record.substring(record.indexOf(':') + 1));
+            String [] arr = record.split(":");
+            if (email.equals(arr[0])) {
+                return Integer.parseInt((arr[1]));
             }
         }
         throw new UserNotFoundException("User with given email doesn't exist");
